b3b9af00c44a5998b2e07992755177e6f3cb5b5b
Ivan-Ignatenko <ivannikolaevich470@gmail.com>
2023-06-21 15:31:17 +0300

    added methods of uploading pictures to firebase on the admin-category page

--- src/app/admin/admin-category/admin-category.component.ts
+++ src/app/admin/admin-category/admin-category.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ICategoryResponce } from 'src/app/shared/interfaces/category/category.interface';
 import { CategoryService } from 'src/app/shared/services/category/category.service';
+import { Storage, deleteObject, getDownloadURL, percentage, ref, uploadBytesResumable } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-admin-category',
@@ -17,9 +18,13 @@ export class AdminCategoryComponent {
   public editStatus = false;
   public formStatus = false;
 
+  public uploadPercent!: number;
+  public isUploaded = false;
+
   constructor(
     private fb: FormBuilder,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private storage: Storage
   ) { }
 
   ngOnInit(): void {
@@ -27,7 +32,7 @@ export class AdminCategoryComponent {
     this.getAllCategories();
   }
 
-  openForm(): void { 
+  openForm(): void {
     this.formStatus = true;
   }
 
@@ -35,7 +40,7 @@ export class AdminCategoryComponent {
     this.categoryForm = this.fb.group({
       name: [null, Validators.required],
       path: [null, Validators.required],
-      imagePath: ['https://www.mundodeportivo.com/alfabeta/hero/2022/01/shingeki-no-kyojin-cosplay-mikasa-ackerman.jpg?width=768&aspect_ratio=16:9&format=nowebp']
+      imagePath: [null, Validators.required]
     });
   }
 
@@ -58,6 +63,7 @@ export class AdminCategoryComponent {
     this.editStatus = false;
     this.categoryForm.reset();
     this.formStatus = false;
+    this.isUploaded = false;
   }
 
   editCategory(category: ICategoryResponce): void {
@@ -69,6 +75,7 @@ export class AdminCategoryComponent {
     });
     this.editStatus = true;
     this.editId = category.id;
+    this.isUploaded = true;
   }
 
   deleteCategory(category: ICategoryResponce): void {
@@ -76,4 +83,55 @@ export class AdminCategoryComponent {
       this.getAllCategories();
     })
   }
+
+  upload(event: any): void {
+    const file = event.target.files[0];
+    this.uploadFile('images', file.name, file)
+      .then(data => {
+        this.categoryForm.patchValue({
+          imagePath: data
+        });
+        this.isUploaded = true;
+      })
+      .catch(err => {
+        console.log(err); 
+      })
+  }
+
+  async uploadFile(folder: string, name: string, file: File | null): Promise<string> {
+    const path = `${folder}/${name}`;
+    let url = '';
+    if (file) {
+      try {
+        const storageRef = ref(this.storage, path);
+        const task = uploadBytesResumable(storageRef, file);
+        percentage(task).subscribe(data => {
+          this.uploadPercent = data.progress;
+        });
+        await task;
+        url = await getDownloadURL(storageRef);
+      } catch (e: any) {
+        console.error(e);
+      }
+    } else {
+      console.log('wrong format');
+    }
+    return Promise.resolve(url);
+  }
+
+  valueByControl(control: string): string{
+    return this.categoryForm.get(control)?.value;
+  }
+
+  deleteImage(): void{
+    const task = ref(this.storage, this.valueByControl('imagePath'));
+    deleteObject(task).then(() => {
+      console.log('file deleted');
+      this.isUploaded = false;
+      this.uploadPercent = 0;
+      this.categoryForm.patchValue({
+        imagePath: null
+      });
+    });
+  };
 }
