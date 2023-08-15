20eda3f5d11854fb2bacf16b960c46278ef38589
Ivan-Ignatenko <ivannikolaevich470@gmail.com>
2023-08-15 15:08:56 +0300

    added a component with a swiper

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,11 +5,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SwiperCarouselComponent } from './swiper-carousel/swiper-carousel.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SwiperCarouselComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/pages/home/swiper-carousel/swiper-carousel.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IActionResponce } from 'src/app/shared/interfaces/actions/actions.interface';
+import { ActionsService } from 'src/app/shared/services/actions/actions.service';
+
+import Swiper, { Navigation } from 'swiper';
+
+@Component({
+  selector: 'app-swiper-carousel',
+  templateUrl: './swiper-carousel.component.html',
+  styleUrls: ['./swiper-carousel.component.scss']
+})
+export class SwiperCarouselComponent {
+
+  public slides: IActionResponce[] = [];
+
+  constructor(
+    private actionsService: ActionsService
+  ){}
+
+  ngAfterViewInit(): void {
+    new Swiper('.swiper-container', {
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      slidesPerView: 1
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllActions();
+  }
+
+  getAllActions(): void {
+    this.actionsService.getAll().subscribe(data => {
+      this.slides = data;
+    })
+  }
+}
